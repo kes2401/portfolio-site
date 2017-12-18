9001b61d0dd5820cf2b5c1e64c083708e13d2ae0
kes2401 <keithscully83@gmail.com>
2017-12-18 20:20:36 +0000

    add scroll functionality to navigation items

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+// Add scroll functionality to navigation items
+$(document).on('click', 'a[href^="#"]', function(event){
+	event.preventDefault();
+
+	$('html, body').animate({
+		scrollTop: $($.attr(this, 'href')).offset().top
+	}, 1000);
+});
+
