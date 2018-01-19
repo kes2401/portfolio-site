83edb8f2697806bcb70e1c040703ecd908681cd8
kes2401 <keithscully83@gmail.com>
2018-01-19 11:06:51 +0000

    start responsive coding

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,29 @@
-// Add scroll functionality to navigation items
-$(document).on('click', 'a[href^="#"]', function(event){
-	event.preventDefault();
+$(document).ready(function() {
 
-	$('html, body').animate({
-		scrollTop: $($.attr(this, 'href')).offset().top
-	}, 1000);
+	// CODE FOR RESPONSIVE NAV BAR
+	// if(window.window.innerWidth <= 740) {
+	// 	$('.nav-bar').removeClass('nav');
+	// 	$('.nav-bar').addClass('nav-mobile');
+	// }
+
+	// Add scroll functionality to navigation items
+	$(document).on('click', 'a[href^="#"]', function(event){
+		event.preventDefault();
+
+		$('html, body').animate({
+			scrollTop: $($.attr(this, 'href')).offset().top
+		}, 1000);
+	});
+
+	// Add animation on hover over skills
+	$('.tech').hover(function(){
+			$(this).addClass('animated tada');
+		}, function() {
+			$(this).removeClass('animated tada');
+		}
+	);
+
+
+
 });
 
-// Add animation on hover over skills
-$('.tech').hover(function(){
-		$(this).addClass('animated tada');
-	}, function() {
-		$(this).removeClass('animated tada');
-	}
-);
-
-// 
